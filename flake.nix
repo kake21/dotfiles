8784582f9fb4e23c7bc0b07b5cfcc2d1b285bd26
Vegard <vegard@steintre.com>
2026-03-12 21:03:43 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Vex NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    astal.url = "github:aylur/astal";
+    ags.url = "github:aylur/ags";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  let
+    system = "x86_64-linux";
+  in
+  {
+    nixosConfigurations.vex = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.vegard = import ./home-manager/home.nix;
+
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+      ];
+    };
+  };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,50 @@
+{ inputs, pkgs, ... }:
+{
+  wayland.windowManager.hyprland.settings = {
+    "$mod" = "SUPER";
+    bind =
+      [
+        "$mod, F, exec, firefox"
+        ", Print, exec, grimblast copy area"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
+  };
+
+  programs.firefox.profiles.vegard = {
+    extensions.packages = with pkgs.firefoxExtensions; [
+      ublock-origin
+      darkreader
+    ];
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+  };
+}
