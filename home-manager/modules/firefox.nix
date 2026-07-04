ade3e0a2f8868df3df797071eff2e5659367b60d
Vegard <vegard@steintre.com>
2026-07-04 15:17:37 +0200

    Fixed firefox config

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -1,12 +1,19 @@
 { config, pkgs, ... }:
 let
   profileUserName = "vegard";
+
+  lock-false = {
+		Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
 in
 {
   programs.firefox = {
     enable = true;
-
-    configPath = "${config.xdg.configHome}/mozilla/firefox";
 
     languagePacks = [ "en-US" ];
 
@@ -14,8 +21,9 @@ in
       isDefault = true;
 
       extensions = {
-        force = true;
-      };
+				force = true;
+			};
+
       search = {
         force          = true;
         default        = "ddg";
@@ -41,6 +49,41 @@ in
 
     policies = {
       DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value= true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+
+      ExtensionSettings = {
+        # uBlock Origin
+      	"uBlock0@raymondhill.net" = {
+      	  install_url = "https://addons.mozilla.org/firefox/downloads/file/4872816/ublock_origin-1.72.0.xpi";
+					installation_mode = "force_installed";
+				};
+
+				# Proton Pass
+				"78272b6fa58f4a1abaac99321d503a20@proton.me" = {
+					install_url = "https://addons.mozilla.org/firefox/downloads/file/4817795/proton_pass-1.37.1.xpi";
+					installation_mode = "force_installed";
+				};
+
+				# Sponsorblock
+				"sponsorBlocker@ajay.app" = {
+					install_url = "https://addons.mozilla.org/firefox/downloads/file/4870235/sponsorblock-6.1.6.xpi";
+					installation_mode = "force_installed";
+				};
+      };
+
+      Preferences = {
+				"browser.formfill.enable" = lock-false;
+			};
     };
   };
 }
