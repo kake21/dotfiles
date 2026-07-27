812d468ef48c569f2f599fa4e70fe700239270f5
Vegard <vegard@steintre.com>
2026-07-27 10:24:10 +0200

    Add trusted users to Nix settings

--- modules/system.nix
+++ modules/system.nix
@@ -33,6 +33,7 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     auto-optimise-store = true;
+    trusted-users = [ "root" "vegard" ];
   };
 
   # Garbage collection
